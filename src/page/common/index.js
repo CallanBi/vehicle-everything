d2afaead2857826552b763813e3d99dbdc99eba5
Moltemort <mcbihv@126.com>
2018-10-31 10:55:43 +0800

    车辆标注bounding box done

--- src/page/common/index.js
+++ src/page/common/index.js
@@ -2,8 +2,8 @@
 * @Author: Moltemort
 * @Date:   2018-10-14 09:57:57
 * @Last Modified by:   Moltemort
-* @Last Modified time: 2018-10-15 18:41:46
+* @Last Modified time: 2018-10-31 10:35:54
 */
 
 require("./index.css");
-console.log("hello common");
+// console.log("hello common");
--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,12 +2,12 @@
 * @Author: Moltemort
 * @Date:   2018-10-14 09:51:32
 * @Last Modified by:   Moltemort
-* @Last Modified time: 2018-10-25 22:23:52
+* @Last Modified time: 2018-10-31 10:36:43
 */
 
 cv = require("opencv.js");
 require("./index.css");
-console.log("hello index.js");
+// console.log("hello index.js");
 
  var body = document.getElementsByTagName("body")[0];
  var videoElement = document.getElementById("video-element");
@@ -24,45 +24,62 @@ console.log("hello index.js");
      let cap = new cv.VideoCapture(videoElement);
      let fgbg = new cv.BackgroundSubtractorMOG2(500, 20, false);
 
-     const FPS = 30;
+     const FPS = 25;
 
      function processVideo() {      
-         let begin = Date.now();
-         cap.read(src);
+        let begin = Date.now();
+        cap.read(src);
 
-         // 高斯平滑处理
-         let ksize = new cv.Size(13, 13);
-         cv.GaussianBlur(src, dst, ksize, 2, 2, cv.BORDER_DEFAULT);
-         // cv.imshow("canvasOutput", dst);
+        // 高斯平滑处理
+        let ksize = new cv.Size(13, 13);
+        cv.GaussianBlur(src, dst, ksize, 2, 2, cv.BORDER_DEFAULT);
+        // cv.imshow("canvasOutput", dst);
 
-         // 灰度处理
-         cv.cvtColor(dst, dst, cv.COLOR_RGBA2GRAY);
-         // cv.imshow("canvasOutput", dst);
-         
+        // 灰度处理
+        cv.cvtColor(dst, dst, cv.COLOR_RGBA2GRAY);
+        // cv.imshow("canvasOutput", dst);
 
-         // 背景差法处理
-         fgbg.apply(dst, dst, 0.2);
-         // cv.imshow("canvasOutput", dst);
 
-         // 二值化处理
-         cv.threshold(dst, dst, 30, 255, cv.THRESH_BINARY);
-         // cv.imshow("canvasOutput", dst);
+        // 背景差法处理
+        fgbg.apply(dst, dst, 0.2);
+        // cv.imshow("canvasOutput", dst);
 
-         // 膨胀处理
-         let Md = cv.Mat.ones(11, 30, cv.CV_8U);
-         let anchor = new cv.Point(-1, -1);
-         cv.dilate(dst, dst, Md, anchor, 1, cv.BORDER_CONSTANT, cv.morphologyDefaultBorderValue());
-         // cv.imshow("canvasOutput", dst);
+        // 二值化处理
+        cv.threshold(dst, dst, 30, 255, cv.THRESH_BINARY);
+        // cv.imshow("canvasOutput", dst);
 
-         // 腐蚀处理
-         let Me = cv.Mat.ones(11, 30, cv.CV_8U);
-         cv.erode(dst, dst, Me, anchor, 1, cv.BORDER_CONSTANT, cv.morphologyDefaultBorderValue());
+        // 膨胀处理
+        let Md = cv.Mat.ones(11, 30, cv.CV_8U);
+        let anchor = new cv.Point(-1, -1);
+        cv.dilate(dst, dst, Md, anchor, 1, cv.BORDER_CONSTANT, cv.morphologyDefaultBorderValue());
+        // cv.imshow("canvasOutput", dst);
 
-         cv.imshow("canvasOutput", dst);
+        // 腐蚀处理
+        let Me = cv.Mat.ones(11, 30, cv.CV_8U);
+        cv.erode(dst, dst, Me, anchor, 1, cv.BORDER_CONSTANT, cv.morphologyDefaultBorderValue());
 
-         // schedule next one.
-         let delay = 1000/FPS - (Date.now() - begin);
-         setTimeout(processVideo, delay);
+
+
+        //绘制轮廓
+        let contours = new cv.MatVector();
+        let hierarchy = new cv.Mat();
+        cv.findContours(dst, contours, hierarchy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE);
+        var boundRects = [];
+        let rectangleColor = new cv.Scalar(255, 0, 0);
+        for (let i = 0; i < contours.size(); ++i) {
+            let cnt = contours.get(i);
+            boundRects.push(cv.boundingRect(cnt));
+            let point1 = new cv.Point(boundRects[i].x, boundRects[i].y);
+            let point2 = new cv.Point(boundRects[i].x + boundRects[i].width, boundRects[i].y + boundRects[i].height);
+            cv.rectangle(src, point1, point2, rectangleColor, 2, cv.LINE_AA, 0);
+        }
+
+        // 输出
+        cv.imshow("canvasOutput", src);
+
+        // schedule next one.
+        let delay = 1000/FPS - (Date.now() - begin);
+        setTimeout(processVideo, delay);
      }
      // schedule first one.
      setTimeout(processVideo, 0);
